58513564caea6affc21ebf21a073704afe960175
Nico Krüger <nico@thedevfactory.com>
2023-04-28 23:29:48 +0000

    JS Code Updates

--- hello_world/core/views.py
+++ hello_world/core/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 
 def index(request):
     
-    a = None
-    a.hello() # Creating an error with an invalid line of code
+    # a = None
+    # a.hello() # Creating an error with an invalid line of code
 
     return render(
         request,
