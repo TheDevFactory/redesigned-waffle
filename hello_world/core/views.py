49f6e5e001d41144ac560933c40650bdc5104044
Nico Krüger <nico@thedevfactory.com>
2023-04-28 22:42:26 +0000

    Updated Code Fixes

--- hello_world/core/views.py
+++ hello_world/core/views.py
@@ -2,11 +2,8 @@ from django.shortcuts import render
 
 def index(request):
     
-    # abc = null
-    # abc.tester()
-
-    # a = None
-    # a.hello() # Creating an error with an invalid line of code
+    a = None
+    a.hello() # Creating an error with an invalid line of code
 
     return render(
         request,
