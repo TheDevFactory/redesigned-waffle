a837f5e991d12848a39f7b555e936c985e1c2b8d
Nico Krüger <nico@thedevfactory.com>
2023-05-01 19:28:57 +0000

    HTML Broken Code

--- hello_world/settings.py
+++ hello_world/settings.py
@@ -136,7 +136,8 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 ROLLBAR = {
-    'access_token': 'b80e8b3e927a4a4d896bc0451b9e7f41',
+    #'access_token': 'b80e8b3e927a4a4d896bc0451b9e7f41',
+    'access_token': '',
     'environment': 'production',
     'code_version': '15eb7cbcff510fbae9b24002b5dd8496b4b6ab76',
     'root': '/workspaces/codespaces-django',
